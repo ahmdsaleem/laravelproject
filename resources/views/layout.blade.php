9083761957b43d72af086c5100579206b2592668
Ahmad Saleem <ahmdsaleem@hotmail.com>
2018-07-19 13:32:00 +0500

    bootstrap fix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="css/main.css" rel="stylesheet" type="text/css" />
+    <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="/css/main.css" rel="stylesheet" type="text/css" />
 
 
 
@@ -18,7 +18,7 @@
     @yield('content')
 </div>
 
-<script src="js/jquery-3.3.1.min.js"> </script>
-<script src="js/bootstrap.min.js"> </script>
+<script src="/js/jquery-3.3.1.min.js"> </script>
+<script src="/js/bootstrap.min.js"> </script>
 </body>
 </html>
